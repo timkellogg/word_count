de16f2c35923adb47f9ad0261c999151c7122b56
timkellogg <tim.kellogg@gmail.com>
2015-08-07 09:20:35 -0700

    handles multiple occurances of multiple words

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -13,4 +13,12 @@ describe('String#count_words') do
 	it("handles multiple words") do 
 		expect("several words here".word_count()).to(eq({"several" => 1, "words" => 1, "here" => 1}))
 	end
+
+	it('handles several appreances of a word') do 
+		expect('many many times'.word_count()).to(eq({"many" => 2, "times" => 1}))
+	end
+
+	it('handles several appearances of several words') do 
+		expect('many many times we say times times times'.word_count()).to(eq({"many" => 2, "times" => 4, "we" => 1, "say" => 1}))
+	end
 end
