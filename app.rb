6d20816ff72e353ec4777849a99ed86f16ee3189
timkellogg <tim.kellogg@gmail.com>
2015-08-07 13:30:22 -0700

    add mobile browsing support

--- app.rb
+++ app.rb
@@ -1,20 +1,15 @@
 require('sinatra')
-require('./lib/word_count')
-require('./lib/find_string')
+require('./lib/*.rb')
 require('sinatra/contrib')
 
 get('/') do 
-	@placeholder_text = "Bacon"
 	erb(:index)
 end
 
 get('/results') do 
-	
 	begin 
 		text                = params.fetch("text")
-
 		@string_to_find     = params.fetch("string_to_find")
-
 		@num_of_occurances  = text.find_string(@string_to_find)
 		@total_words        = text.word_count().values.inject(&:+)
 		@counted_words      = text.word_count().sort_by {|key, value| value }.reverse()
