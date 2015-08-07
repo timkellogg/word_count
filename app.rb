f57034e71b39e53e43445b3d71d687aadb233a24
timkellogg <tim.kellogg@gmail.com>
2015-08-07 13:09:06 -0700

    improve ui on result page to distinguish phrase lookup

--- app.rb
+++ app.rb
@@ -12,7 +12,9 @@ get('/results') do
 	
 	begin 
 		text                = params.fetch("text")
+
 		@string_to_find     = params.fetch("string_to_find")
+
 		@num_of_occurances  = text.find_string(@string_to_find)
 		@total_words        = text.word_count().values.inject(&:+)
 		@counted_words      = text.word_count().sort_by {|key, value| value }.reverse()
