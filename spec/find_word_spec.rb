7dcd3d34ff91331bc1402802d9fd0e776d411155
timkellogg <tim.kellogg@gmail.com>
2015-08-07 11:12:17 -0700

    it handles multiple occurances of a word

--- spec/find_word_spec.rb
+++ spec/find_word_spec.rb
@@ -11,8 +11,12 @@ describe('String#find_word') do
 		expect('something'.find_word('nothing')).to(eq(0))
 	end
 
-	# it('handles an empty string') do 
-	# 	expect(''.find_word('one').to(eq()))
-	# end
+	it('handles an empty string') do 
+		expect(''.find_word('one')).to(eq(0))
+	end
+
+	it('handles multiple occurances of a word in a string') do 
+		expect('i need to know what you need to know'.find_word('need')).to(eq(2))
+	end
 
 end
