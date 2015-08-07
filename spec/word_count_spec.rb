a1f49b17874a8f0c205cc086663921d8538db45c
timkellogg <tim.kellogg@gmail.com>
2015-08-07 09:14:26 -0700

    it handles several words in a string

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -10,4 +10,7 @@ describe('String#count_words') do
 		expect("word".word_count()).to(eq({"word" => 1}))
 	end
 
+	it("handles multiple words") do 
+		expect("several words here".word_count()).to(eq({"several" => 1, "words" => 1, "here" => 1}))
+	end
 end
