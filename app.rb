931cba70588a4143173eb44c8d882a3f73b8ac71
timkellogg <tim.kellogg@gmail.com>
2015-08-07 13:58:08 -0700

    fix lib loading error

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require('sinatra')
-require('./lib/*.rb')
+require('./lib/find_string.rb')
+require('./lib/word_count.rb')
 require('sinatra/contrib')
 
 get('/') do 
@@ -8,11 +9,11 @@ end
 
 get('/results') do 
 	begin 
-		text                = params.fetch("text")
+		@text               = params.fetch("text")
 		@string_to_find     = params.fetch("string_to_find")
-		@num_of_occurances  = text.find_string(@string_to_find)
-		@total_words        = text.word_count().values.inject(&:+)
-		@counted_words      = text.word_count().sort_by {|key, value| value }.reverse()
+		@num_of_occurances  = @text.find_string(@string_to_find)
+		@total_words        = @text.word_count().values.inject(&:+)
+		@counted_words      = @text.word_count().sort_by {|key, value| value }.reverse()
 		erb(:results)
 	rescue => e  
 		erb(:index)
